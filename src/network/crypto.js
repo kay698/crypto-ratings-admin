925bb0cd4df97c7f647ecd133a3cc26bb004f0f8
kay698 <ogbukakelechi20@gmail.com>
2022-02-23 21:34:53 +0100

    cryto edit added

--- src/network/crypto.js
+++ src/network/crypto.js
@@ -1,34 +1,28 @@
-import instance from "./axios"
+import instance from "./axios";
 
 export const addCrypto = async function (payload) {
-  const { data } = await instance.post(`/crypto/create`, payload)
-  return data
-}
+  const { data } = await instance.post(`/crypto/create`, payload);
+  return data;
+};
 
-export const updateCrypto = async function (payload, cryptoId) {
-  const { data } = await instance.put(`/crypto/update/${cryptoId}`, payload)
-  return data
-}
-export async function getAllGiftCardCategories(payload) {
-  const { data } = await instance.get(
-    `/gift-cards?page=${payload.page}&perPage=${payload.perPage}`
-  )
-  return data
-}
+export const updateCrypto = async function (payload) {
+  const { data } = await instance.put(`/crypto/update`, payload);
+  return data;
+};
 
 export const getAllCrypto = async function (payload) {
   const data = await instance.get(
     `/crypto?page=${payload.page}&perPage=${payload.perPage}`
-  )
-  return data
-}
+  );
+  return data;
+};
 
 export const getSingleCrypto = async function (cryptoId) {
-  const { data } = await instance.get(`/crypto/${cryptoId}`)
-  return data
-}
+  const { data } = await instance.get(`/crypto/${cryptoId}`);
+  return data;
+};
 
 export const deleteCrypto = async function (payload) {
-  const { data } = await instance.delete(`/crypto/delete`, payload)
-  return data
-}
+  const { data } = await instance.delete(`/crypto/delete`, payload);
+  return data;
+};
