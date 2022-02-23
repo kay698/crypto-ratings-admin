d74851d015b3caf8aed0aed4419a0d323de6acd5
George Favour C <phavorsparks@gmail.com>
2022-02-23 15:26:38 +0100

    fetch, display and add crypto

--- src/pages/Dashboard/Crypto/index.jsx
+++ src/pages/Dashboard/Crypto/index.jsx
@@ -1,19 +1,26 @@
-import React, { useState } from "react";
-import DashboardLayout from "../../../components/Layout";
-import Input from "../../../components/TextField";
-import { FlexibleDiv } from "../../../components/Box/styles";
-import { Typography, Popover } from "antd";
-import Button from "../../../components/Button";
-import CustomTable from "../../../components/Table";
-import { BiDotsVerticalRounded, BiSearch } from "react-icons/bi";
-import { CryptoStyles } from "./styles";
-import { RiDeleteBin6Line } from "react-icons/ri";
-import { FiEdit } from "react-icons/fi";
+import React, { useState } from "react"
+import DashboardLayout from "../../../components/Layout"
+import Input from "../../../components/TextField"
+import Select from "../../../components/Select"
+import { FlexibleDiv } from "../../../components/Box/styles"
+import { Typography, Popover, notification, Form } from "antd"
+import Button from "../../../components/Button"
+import CustomTable from "../../../components/Table"
+import { BiDotsVerticalRounded, BiSearch } from "react-icons/bi"
+import { CryptoStyles } from "./styles"
+import { RiDeleteBin6Line } from "react-icons/ri"
+import { FiEdit } from "react-icons/fi"
+import { addCrypto, getAllCrypto } from "../../../network/crypto"
+import { ModalWrapper } from "../../../components/ModalStylesWrap"
+import { SmileOutlined, LoadingOutlined } from "@ant-design/icons"
 
 function Crypto() {
-  const [searching, setSearching] = useState(false);
-  const [searchResults, setSearchResults] = useState();
-  const [showDrawer, setShowDrawer] = useState(true);
+  const [searching, setSearching] = useState(false)
+  const [searchResults, setSearchResults] = useState()
+  const [showDrawer, setShowDrawer] = useState(true)
+  const [showAddNewCrypto, setShowAddNewCrypto] = useState(false)
+  const [form] = Form.useForm()
+  const [isLoading, setIsLoading] = useState(false)
 
   const content = (
     <>
@@ -26,20 +33,23 @@ function Crypto() {
         Delete
       </p>
     </>
-  );
+  )
 
   const columns = [
     {
-      title: "Categories",
-      dataIndex: "categories",
-      key: "categories",
-      render: (card) => card.cryptocurrency,
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
     },
 
     {
-      title: "Crypto",
-      dataIndex: "crypto",
-      render: (card) => card.Rate,
+      title: "Amount",
+      dataIndex: "amount",
+    },
+
+    {
+      title: "Currency",
+      dataIndex: "currency",
     },
 
     {
@@ -65,7 +75,42 @@ function Crypto() {
         </Popover>
       ),
     },
-  ];
+  ]
+
+  // create card category
+  async function handleCreateNewCrypto(values) {
+    setIsLoading(true)
+    const payload = {
+      ...values,
+    }
+    try {
+      const data = await addCrypto(payload)
+      setShowAddNewCrypto(false)
+      notification.open({
+        message: "Success",
+        description: "Crypto added",
+        icon: <SmileOutlined style={{ color: "green" }} />,
+      })
+      setIsLoading(false)
+    } catch (error) {
+      if (error.response) {
+        notification.open({
+          message: "Error",
+          description: error.response.data.message,
+          icon: <SmileOutlined style={{ color: "red" }} />,
+        })
+        setIsLoading(false)
+      } else {
+        notification.open({
+          message: "Error",
+          description:
+            "There was an issue with your network. Pls try again later",
+          icon: <SmileOutlined style={{ color: "red" }} />,
+        })
+        setIsLoading(false)
+      }
+    }
+  }
 
   // const handleSearch = async (value) => {
   //   if (value.searchResults === "") {
@@ -106,20 +151,96 @@ function Crypto() {
                 radius="8px"
                 prefix={<BiSearch />}
               />
-              <Button height="50px" boxShadow="none">
+              <Button
+                height="50px"
+                boxShadow="none"
+                onClick={() => setShowAddNewCrypto(true)}
+              >
                 + Add New Crypto
               </Button>
             </FlexibleDiv>
           </FlexibleDiv>
           <CustomTable
-            func=""
+            func={getAllCrypto}
             columns={columns}
             searchResults={searchResults}
           />
         </FlexibleDiv>
       </CryptoStyles>
+
+      <ModalWrapper
+        visible={showAddNewCrypto}
+        footer={null}
+        closable={true}
+        onCancel={() => setShowAddNewCrypto(false)}
+      >
+        <FlexibleDiv flexDir="column">
+          <Typography.Title level={5}>Add New Crypto</Typography.Title>
+          <FlexibleDiv margin="30px 0">
+            <Form
+              form={form}
+              onFinish={handleCreateNewCrypto}
+              style={{ width: "100%" }}
+            >
+              <Form.Item
+                name="title"
+                rules={[
+                  {
+                    required: true,
+                    message: "This field is required",
+                  },
+                ]}
+              >
+                <Input
+                  placeholder="Crypto Name"
+                  background="#F5FCFF"
+                  height="50px"
+                />
+              </Form.Item>
+              <Form.Item
+                name="amount"
+                rules={[
+                  {
+                    required: true,
+                    message: "This field is required",
+                  },
+                ]}
+              >
+                <Input
+                  placeholder="Amount"
+                  background="#F5FCFF"
+                  height="50px"
+                />
+              </Form.Item>
+              <Form.Item
+                name="currency"
+                rules={[
+                  {
+                    required: true,
+                    message: "This field is required",
+                  },
+                ]}
+              >
+                <Select
+                  placeholder="Currency"
+                  background="#F5FCFF"
+                  height="50px"
+                >
+                  <Select.Option value="USD">USD</Select.Option>
+                  <Select.Option value="EURO">EURO</Select.Option>
+                  <Select.Option value="POUNDS">POUNDS</Select.Option>
+                </Select>
+              </Form.Item>
+              <Button type="primary" htmlType="submit" width="100%">
+                {isLoading && <LoadingOutlined />}
+                Save
+              </Button>
+            </Form>
+          </FlexibleDiv>
+        </FlexibleDiv>
+      </ModalWrapper>
     </DashboardLayout>
-  );
+  )
 }
 
-export default Crypto;
+export default Crypto
