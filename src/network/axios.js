11b220e5e72c127bd5e412e5877a2757a6642e25
George Favour C <phavorsparks@gmail.com>
2022-02-23 15:25:45 +0100

    network requests from crypto data

--- src/network/axios.js
+++ src/network/axios.js
@@ -1,10 +1,10 @@
-import axios from "axios";
+import axios from "axios"
 
 const instance = axios.create({
   baseURL: "https://iraters-backend.herokuapp.com",
   headers: {
     Authorization: sessionStorage.getItem("admin_token") || null,
   },
-});
+})
 
-export default instance;
+export default instance
---
+++ src/network/crypto.js
@@ -0,0 +1,34 @@
+import instance from "./axios"
+
+export const addCrypto = async function (payload) {
+  const { data } = await instance.post(`/crypto/create`, payload)
+  return data
+}
+
+export const updateCrypto = async function (payload, cryptoId) {
+  const { data } = await instance.put(`/crypto/update/${cryptoId}`, payload)
+  return data
+}
+export async function getAllGiftCardCategories(payload) {
+  const { data } = await instance.get(
+    `/gift-cards?page=${payload.page}&perPage=${payload.perPage}`
+  )
+  return data
+}
+
+export const getAllCrypto = async function (payload) {
+  const data = await instance.get(
+    `/crypto?page=${payload.page}&perPage=${payload.perPage}`
+  )
+  return data
+}
+
+export const getSingleCrypto = async function (cryptoId) {
+  const { data } = await instance.get(`/crypto/${cryptoId}`)
+  return data
+}
+
+export const deleteCrypto = async function (payload) {
+  const { data } = await instance.delete(`/crypto/delete`, payload)
+  return data
+}
